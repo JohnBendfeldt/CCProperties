18504a03fe4d48c01c4f0c2193b8a95e120c3ea5
John Bendfeldt <jfbendfeldt@gmail.com>
2017-05-28 23:33:24 -0500

    added firebase connection

---
+++ assets/javascript/ccproperties.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDAnYNQ-WX_oQR1tdATPCcW6554s8JxwEQ",
+    authDomain: "ccproperties-d20e7.firebaseapp.com",
+    databaseURL: "https://ccproperties-d20e7.firebaseio.com",
+    projectId: "ccproperties-d20e7",
+    storageBucket: "ccproperties-d20e7.appspot.com",
+    messagingSenderId: "281197146032"
+  };
+  firebase.initializeApp(config);
